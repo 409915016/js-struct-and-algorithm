7dd8c91861edc66ef86ce564d675721325c79146
mather <misaka@misaka.im>
2019-11-30 20:52:46 +0800

    add Set

--- main.js
+++ main.js
@@ -111,3 +111,56 @@ function hotPotato(nameList, num){
 // list.append(10)
 // console.log(list.toString())
 //单项链表
+
+
+
+//集合
+let setA = new Set();
+setA.add(1);
+setA.add(2);
+setA.add(3);
+
+let setB = new Set();
+setB.add(2);
+setB.add(3);
+setB.add(4);
+
+//并集
+let unionAb = new Set();
+for (let x of setA) unionAb.add(x);
+for (let x of setB) unionAb.add(x);
+//console.log(unionAb)
+
+
+//交集
+let intersection = function (setA, setB) {
+    let intersectionSet = new Set()
+
+    for(let x of setA){
+        if(setB.has(x)){
+            intersectionSet.add(x)
+        }
+    }
+
+    return intersectionSet
+}
+let intersectionAB = intersection(setA, setB);
+//console.log(intersectionAB);
+
+
+//差集
+let difference = function (setA, setB) { 
+    let differenceSet = new Set()
+
+    for(let x of setA){
+        if(!setB.has(x)){
+            differenceSet.add(x)
+        }
+    }
+
+    return differenceSet
+}
+let differenceAB = difference(setA, setB)
+//console.log(differenceAB)
+
+
